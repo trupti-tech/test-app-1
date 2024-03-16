f6345e30d2fb3a8d3376372fe06bf6f5dc7b7078
trupti-tech <trupti.techworks@gmail.com>
2024-03-16 19:32:36 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import express from "express";
+
+let conn = await mongoose.connect("mongodb+srv://hitarthganatra123:abcd123@testcluster.mpoypll.mongodb.net/");
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
